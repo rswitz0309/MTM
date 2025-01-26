259046312fa1afd1e1a7a04922b068a705195357
GAROLF05 <88960158+GAROLF05@users.noreply.github.com>
2025-01-26 08:13:35 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeaderSection from "./components/HeaderSection";
+import FeatureSection from "./components/FeatureSection";
+
+
+
+const App = () =>{
+  return(
+    <>
+      <Navbar/>
+      <HeaderSection/>
+      <FeatureSection/>
+      
+          
+
+
+      
+    </>
+  );
+};
+
+export default App
